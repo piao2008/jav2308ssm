59b61d8b740e09c19006b63cff162a4606c8ac30
piao2080 <76135481@qq.com>
2023-12-20 16:42:08 +0800

    2023-12-20 测试2--cat

---
+++ src/main/java/com/as/ssm/controller/GoodsController.java
@@ -0,0 +1,14 @@
+package com.as.ssm.controller;
+
+/**
+ * 描述：
+ * 创建人：飘
+ * 创建时间：2023/12/20
+ **/
+
+public class GoodsController {
+    public  void testGoods()
+    {
+        System.out.println("goods....");
+    }
+}
