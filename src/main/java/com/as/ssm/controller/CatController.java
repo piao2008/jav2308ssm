34dcdec2ea71b5e303b28d690680241df856dc33
piao2080 <76135481@qq.com>
2023-12-20 16:41:03 +0800

    2023-12-20 测试2--cat

---
+++ src/main/java/com/as/ssm/controller/CatController.java
@@ -0,0 +1,21 @@
+package com.as.ssm.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * 描述：
+ * 创建人：飘
+ * 创建时间：2023/12/20
+ **/
+@Controller
+@RequestMapping("/cat")
+public class CatController {
+
+    public  void test()
+    {
+        System.out.println("cat..");
+    }
+}
+
+
